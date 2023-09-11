50424218677678ec148002aaf7139a06b8c09432
dtrain1313 <144736490+dtrain1313@users.noreply.github.com>
2023-09-12 09:18:39 +1000

    Update streamlit_app.py
    added Fruit Smoothie title

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.title('Build Your Own Fruit Smoothie')
